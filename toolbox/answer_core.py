04e622c51f2fd913946ee5fd7a6aa8bc850a9b4b
Meow Lu <ttsmcpe@gmail.com>
2023-07-26 17:18:52 +0800

    Fix : 採用吞吐量更大的模型

--- toolbox/answer_core.py
+++ toolbox/answer_core.py
@@ -12,7 +12,10 @@ anthropic = Anthropic(api_key=anthropic_api_key)
 ## openai
 import openai
 openai_api_key = os.getenv("openai_api_key")
+openai_gpt_model = "gpt-3.5-turbo-16k"
+
 openai.api_key = openai_api_key
+
 
 ## RoBERTa
 # https://huggingface.co/uer/roberta-base-chinese-extractive-qa
@@ -32,7 +35,7 @@ bert_nlp = pipeline('question-answering', model=bert_model, tokenizer=bert_token
 def qa_by_anthropic(source_content, question):
     try:
         completion = anthropic.completions.create(
-            model="claude-1",
+            model="claude-2",
             max_tokens_to_sample=300,
             prompt= f"""Human: 
                 我會給你一份檔案。 然後我會向你提問， 利用檔案內的內容來回答。 這是檔案內容：      
@@ -55,13 +58,16 @@ def qa_by_anthropic(source_content, question):
 
 def qa_by_openai(source_content,question):
     try:
+        # https://platform.openai.com/docs/models/gpt-3-5
+        # https://beta.openai.com/docs/api-reference/completions/create
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
+            model = openai_gpt_model,
+            messages= [
                 {"role": "system", "content": f"我會給你一份檔案。 然後我會向你提問， 利用檔案內的內容來回答。如果發現內容無法回答則回覆「無法提供最佳答案」。這是檔案內容：{source_content}" },
                 {"role": "user", "content": f"採用我提供的資料用繁體中文嘗試回答：{question}，這是單次問答無須說明開頭與結尾。" }
             ],
-            temperature=0,
+            temperature=0, # 嘗試固定回答
+            max_tokens=500, # 最多回答 500 tokens
         )
         return {
             "state": True,
